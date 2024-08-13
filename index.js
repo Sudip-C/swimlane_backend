fc13ac38baa438ef841c8732a48db27047aa5755
Sudip Chowdhury <rexalex826@gmail.com>
2024-08-13 21:15:34 +0530

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-const jsonServer = require('json-server');
-const cors = require('cors');
-const server = jsonServer.create();
-const router = jsonServer.router( 'db.json');
-const middlewares = jsonServer.defaults();
+const jsonserver= require("json-server");
+const server= jsonserver.create()
+const router= jsonserver.router("db.json")
+const middleware= jsonserver.defaults()
+const port =process.env.PORT ||8080;
+server.use(middleware)
+server.use(router)
 
-// Enable CORS for all routes
-server.use(cors());
-
-// Set default middlewares (logger, static, cors, and no-cache)
-server.use(middlewares);
-
-// Use default router
-server.use(router);
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+server.listen(port)
